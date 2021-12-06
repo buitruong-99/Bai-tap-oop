74d76a61f1b27f4e5e295cdc13d340685c40abe9
Bùi Văn Trường <truongbui151930@gmail.com>
2021-12-06 10:11:34 +0700

    update

--- src/com/bvtruong/lab05/QLCB.java
+++ src/com/bvtruong/lab05/QLCB.java
@@ -1,38 +1,76 @@
 package com.bvtruong.lab05;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class QLCB {
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
+        CanBo canBo;
+//        Scanner scanner = new Scanner(System.in);
         int chucnang;
         do {
-            System.out.println("1. Nhập thông tin cán bộ.");
-            System.out.println("2. Tìm kiếm theo họ tên.");
-            System.out.println("3. Hiển thị thông tin về danh sách cán bộ.");
-            System.out.println("0. THOÁT.");
-            System.out.print("Chọn chức năng: ");
+            System.out.println("1. Nhập thông tin mới cho cán bộ ");
+            System.out.println("2. Tìm kiếm theo họ tên ");
+            System.out.println("3. Hiển thị thông tin về danh sách các cán bộ ");
+            System.out.println("0. Thoát");
+            System.out.print("Chọn chức năng chính");
             chucnang = new Scanner(System.in).nextInt();
 
             switch (chucnang) {
                 case 1:
                     int chucnang1;
                     do {
-                        System.out.println("1. Nhập công nhân.");
-                        System.out.println("2. Nhập kỹ sư.");
-                        System.out.println("3. Nhập nhân viên.");
-                        System.out.println("0. THOÁT.");
-                        System.out.print("Chọn chức năng: ");
+                        System.out.println("1. Nhập thông tin công nhân ");
+                        System.out.println("2. Nhập thông tin kỹ sư ");
+                        System.out.println("3. Nhập thông tin nhân viên");
+                        System.out.println("0. Thoát");
+                        System.out.println("Chọn chức năng nhập thông tin cho cán bộ");
                         chucnang1 = new Scanner(System.in).nextInt();
-                    }while (chucnang1 != 0);
+                        switch (chucnang1) {
+                            case 1:
+                                System.out.println("Nhập thông tin công nhân");
+                                canBo = new CongNhan();
+                                canBo.input();
+                                list.add(canBo);
+                                break;
+                            case 2:
+                                System.out.println("Nhập thông tin kỹ sư");
+                                canBo = new KySu();
+                                canBo.input();
+                                list.add(canBo);
+                                break;
+                            case 3:
+                                System.out.println("Nhập thông tin nhân viên");
+                                canBo = new NhanVien();
+                                canBo.input();
+                                list.add(canBo);
+                        }
+                    }
+                    while (chucnang1 != 0);
+                    System.out.println("Thoát chức năng 1");
                     break;
-                default:
-                    throw new AssertionError();
-            }
+                case 2:
+                    System.out.println("Tìm kiếm theo họ tên");
+                    System.out.print("Nhập họ tên cần tìm kiếm: ");
+                    String hoten = new Scanner(System.in).nextLine();
+                    CanBo cb;
+                    for (Object object: list) {
+                        cb = (CanBo) object;
+                        if (cb.getHoten().equalsIgnoreCase(hoten)) {
+                            System.out.println(cb);
+                        }
 
+                    }
+                    break;
+                case 3:
+                    System.out.println("Hiển thị danh sách cán bộ");
+                    System.out.println(list);
+                    break;
+            }
         }
         while (chucnang != 0);
-        System.out.println("Chương trình kết thúc");
+        System.out.println("Chương trình đã kết thúc");
     }
 }
